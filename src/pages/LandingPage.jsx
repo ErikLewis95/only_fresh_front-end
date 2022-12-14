e741f36814451c3caee8854798b5303b92537d99
fachrizahidaya <fachriza.theblues@gmail.com>
2022-12-14 14:11:47 +0700

    test

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,19 @@
+import { Box, Center } from "@chakra-ui/react";
+
+export const LandingPage = () => {
+  return (
+    <div>
+      <Center>
+        <Box bgColor="teal" w="500px" h="820px">
+          <Box bgColor={"salmon"} w="500px" h="70px">
+            Landing
+          </Box>
+
+          <Box bgColor={"black"} w="500px" h="61px">
+            Landing
+          </Box>
+        </Box>
+      </Center>
+    </div>
+  );
+};
